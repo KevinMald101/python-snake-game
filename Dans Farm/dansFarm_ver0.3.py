72d203a0d8aaac1f41b58915be12a7aaf37cbac7
Kevin M <93846113+KevinMald101@users.noreply.github.com>
2023-03-30 21:04:15 -0400

    Grass Options Display closes #5
    1. Made the grass option display (god) the way i wanted it to display the grass. With brackets around it.
    
    2. By using a for loop i added brackets around the items in the list of grass. Then added the new string to another string that'll get all the grasses and display it.
    
    I feel like i need to learn how to explain stuff more clearer.

---
+++ Dans Farm/dansFarm_ver0.3.py
@@ -0,0 +1,67 @@
+# @KevinMald101 Github
+
+import time
+import random
+
+# Play the game
+play = True
+while play == True:
+  # Stuff before the game starts
+  # How many grass should there be
+  num_of_grass = random.randint(3,10)
+  game_dictionary = {}
+  for num in range(1, num_of_grass):
+    grass = "grass" + str(num)
+    # Choosing where the animals are
+    pick_animal = random.randint(1,2)
+    # If num i one then sheep
+    if pick_animal == 1:
+      animal = "sheep"
+    # Else pick_animal is 2 then make it a wolf
+    else:
+      animal = "wolf"
+    # Check if there's already a wolf
+    if "wolf" in game_dictionary.values():
+      # If there is then make the animal a sheep since for now only 1 wolf
+      animal = "sheep"
+    # Add the grass with what's hiding in it to the grass dictionary 
+    game_dictionary.update({grass: animal})
+  # In case there's no wolf, add one
+  if "wolf" not in game_dictionary.values():
+    # The index to where to add the wolf
+    ran_num = random.randint(2,num_of_grass)
+    grass = "grass" + str(ran_num)
+    # Change the value of the grass a wolf
+    game_dictionary[grass] = "wolf"
+
+  # When the game starts
+  print("As you walk around you farm, you come across some wolf tracks leading toward your hoard of sheep.")
+  time.sleep(5)
+  print("You take out your rifle and explore the thick grass.")
+  time.sleep(4)
+  all_grass = game_dictionary.keys()
+  # Empty list to add the brackets around the grass
+  grass_brackets = ""
+  # The string to display options
+  grass_display = "|"
+  # For loop to add the brackets, and add the new string to an existing string
+  for grasses in all_grass:
+    grass_brackets = " " + grasses + " |"
+    grass_display += grass_brackets
+  # Print all options
+  print(grass_display)
+  # Ask user for input
+  search = input("Where do you search? ")
+  # Ask if you want to play again
+  while play == True:
+    continue_game = input("Would you like to play again? (y/n)\n").lower()
+    if continue_game == "y":
+      print("Alright wolf slayer!")
+      break
+    # If n then make play false
+    elif continue_game == "n":
+      play = False
+    else:
+      print("Not a valid option")
+
+# @KevinMald101 Github
